45d55c4a4b2a5de818883186d4fa493b146723db
h-pgy <hpougy@gmail.com>
2023-03-16 21:13:11 -0300

    folder env variables

--- core/config.py
+++ core/config.py
@@ -3,3 +3,4 @@ from .utils.env import get_env_var
 USER_EMAIL=get_env_var('USER_EMAIL')
 PASSWORD=get_env_var('PASSWORD')
 API_HOST=get_env_var('API_HOST')
+ORIGINAL_DATA_FOLDER=get_env_var('ORIGINAL_DATA_FOLDER')
