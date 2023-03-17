43fe1f7b167fb11d02b99b373bd463d005fc4fa4
h-pgy <hpougy@gmail.com>
2023-03-17 17:33:42 -0300

    find id indicador

--- core/transform/__init__.py
+++ core/transform/__init__.py
@@ -1,3 +1,11 @@
 from .id_meta import FindIdMeta
 
 find_id_meta = FindIdMeta()
+
+from .id_iniciativa import FindIdIniciativa
+
+find_id_iniciativa = FindIdIniciativa()
+
+from .id_indicador import FindIdIndicador
+
+find_id_indicador = FindIdIndicador()
